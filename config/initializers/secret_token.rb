eb84f2bbe53529f4498e76a9d5ddf95dbf3c89dc
Ryan J. Geyer <me@ryangeyer.com>
2014-02-24 21:19:48 -0800

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RsCiAppRails::Application.config.secret_key_base = '1aba9e834a9aef7698e7cd4825eab26bdb02b0c8514e1ca77e2ba9a0a2cb35e1984fe71a6d4178f4dd051d0e49db9daa0cbe862df785377fe90984ebabdff9cb'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+RsCiAppRails::Application.config.session_store :cookie_store, key: '_rs_ci_app_rails_session'
