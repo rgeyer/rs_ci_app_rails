d8fefed103dff207657177a7311f223fde352b6d
Ryan J. Geyer <me@ryangeyer.com>
2014-02-25 13:58:26 -0800

    Add index page which shows git SHA, and enable travis tests

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,10 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  before_filter :get_commit
+
+  def get_commit
+    @commit = %x{git show --pretty=%H}.lines.first.chomp
+  end
 end
